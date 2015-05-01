aaf0e6faa1fea1cb3a9d502a3b91f018049ed0d0
Thomas Eberle <Thomas@tommyelroy.com>
2015-05-01 22:22:42 +0200

    First test to try out Travis and headless support.

---
+++ src/test/java/elroy/codefactory/swing/gui/complete/test/ApplicationTest.java
@@ -0,0 +1,14 @@
+package elroy.codefactory.swing.gui.complete.test;
+
+import org.junit.Test;
+
+import elroy.codefactory.swing.gui.complete.Application;
+
+public class ApplicationTest {
+
+	
+	@Test
+	public void testApplication(){
+		Application.main(null);
+	}
+}
